aebae1fd4aa0ac01e44dbb465ce76876e6cf36e4
sambunarayan <soyou30257@gmail.com>
2022-08-14 22:47:56 +0900

    First commit

---
+++ src/main/java/jp/co/jeus_blog/WebAppInitializer.java
@@ -0,0 +1,21 @@
+package jp.co.jeus_blog;
+
+import jp.co.jeus_blog.config.ServletConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {ServletConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
