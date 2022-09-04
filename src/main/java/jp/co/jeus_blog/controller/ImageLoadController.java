462ed7b0bd3fcda6c434278f2284e26f67e09f5c
sambunarayan <soyou30257@gmail.com>
2022-09-04 22:51:51 +0900

    Add comments

--- src/main/java/jp/co/jeus_blog/controller/ImageLoadController.java
+++ src/main/java/jp/co/jeus_blog/controller/ImageLoadController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * Image load controller
+ */
 @Log4j2
 @RequestMapping("blog/image")
 @Controller
