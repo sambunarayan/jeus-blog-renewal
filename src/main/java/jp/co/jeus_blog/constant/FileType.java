298ffe3df38cfea4f23fd201625d365aeae50640
sambunarayan <soyou30257@gmail.com>
2022-09-14 23:11:34 +0900

    Add comments

--- src/main/java/jp/co/jeus_blog/constant/FileType.java
+++ src/main/java/jp/co/jeus_blog/constant/FileType.java
@@ -1,6 +1,15 @@
 package jp.co.jeus_blog.constant;
 
+/**
+ * File type
+ */
 public enum FileType {
+    /**
+     * Image file
+     */
     IMAGE,
+    /**
+     * Logo image file
+     */
     LOGO_IMAGE,;
 }
