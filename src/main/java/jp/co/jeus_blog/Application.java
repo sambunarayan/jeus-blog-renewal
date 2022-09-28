9429c7b377721ba5fad7afa77be316319c83a800
Jaewoo Kim <soyou30257@gmail.com>
2022-09-28 23:00:00 +0900

    Add comments

--- src/main/java/jp/co/jeus_blog/Application.java
+++ src/main/java/jp/co/jeus_blog/Application.java
@@ -3,6 +3,9 @@ package jp.co.jeus_blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring boot application
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
