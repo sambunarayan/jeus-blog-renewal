ac20f404e5ae63e83590e2e443c0db7ef9191011
Jaewoo Kim <soyou30257@gmail.com>
2022-10-12 22:44:54 +0900

    Add css and js for portfolio

--- src/main/java/jp/co/jeus_blog/controller/PortfolioController.java
+++ src/main/java/jp/co/jeus_blog/controller/PortfolioController.java
@@ -1,7 +1,13 @@
 package jp.co.jeus_blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PortfolioController {
+
+    @GetMapping("/portfolio")
+    public String getPage() {
+        return "portfolio";
+    }
 }
